b7804c47c5d37854f4a3f9ee2a07e0c3dd95f2af
seladb <pcapplusplus@gmail.com>
2017-05-25 13:19:07 +0300

    Update build-dpdk.sh

--- Deploy/build-dpdk.sh
+++ Deploy/build-dpdk.sh
@@ -1,5 +1,5 @@
-wget http://fast.dpdk.org/rel/dpdk-16.11.1.tar.gz
-tar xvzf dpdk-16.11.1.tar.gz
-cd dpdk-stable-16.11.1
+wget http://fast.dpdk.org/rel/dpdk-$1.tar.gz
+tar xvzf dpdk-$1.tar.gz
+cd $2
 make config CC=gcc T=x86_64-native-linuxapp-gcc
 make CC=gcc
